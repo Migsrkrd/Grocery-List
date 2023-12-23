98bea48fa914ce52abf3b878ed1ca4051afbe607
Mike <mreickcastillero@gmail.com>
2023-12-23 15:35:24 -0800

    added render build to root package

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,25 +1,25 @@
 const Footer = () => {
   return (
     <footer>
-  <div class="footer-content">
-    <div class="footer-section">
+  <div className="footer-content">
+    <div className="footer-section">
       <h2>Contact Us</h2>
       <p>Email: info@grocease.com</p>
       <p>Phone: (123) 456-7890</p>
     </div>
-    <div class="footer-section">
+    <div className="footer-section">
       <h2>Follow Us</h2>
       <p>Facebook</p>
       <p>Twitter</p>
       <p>Instagram</p>
     </div>
-    <div class="footer-section">
+    <div className="footer-section">
       <h2>Links</h2>
       <p><a href="#">Terms of Service</a></p>
       <p><a href="#">Privacy Policy</a></p>
     </div>
   </div>
-  <div class="footer-bottom">
+  <div className="footer-bottom">
     <p>&copy; 2023 Grocease. All rights reserved.</p>
   </div>
 </footer>
