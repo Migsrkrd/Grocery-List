39f7820d7f31e93dd3704e12670b7a7725418b7f
Mike <mreickcastillero@gmail.com>
2023-12-23 14:47:51 -0800

    adjusted mongodb to mongodb atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/GrocereaseDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/GrocereaseDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 
 module.exports = mongoose.connection;
